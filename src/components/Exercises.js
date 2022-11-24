4e1716cbbc71a412f176ce114769308dccbbd24d
Johnny Briones <jbrrii91@gmail.com>
2022-11-24 08:04:37 -0500

    create exercises, herobanner, searchexercises componenets, navbar and home files

---
+++ src/components/Exercises.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export const Exercises = () => {
+    return (
+        <div>Exercises</div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box } from '@mui/material';
+import { HeroBanner } from '../components/HeroBanner';
+import { SearchExercises } from '../components/SearchExercises';
+import { Exercises } from '../components/Exercises';
 
 
 export const Home = () => {
     return (
-        <div>Home</div>
+        <Box>
+            <HeroBanner />
+            <SearchExercises />
+            <Exercises />
+        </Box>
     )
 }
 
